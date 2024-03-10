19fc1f257b7f72c34161dab54013a7a0f5135815
grestavia <blahsheepuwu@gmail.com>
2024-03-10 18:56:55 +0700

    fix: nested view

--- tools/StackNavigator.js
+++ tools/StackNavigator.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import Home from '../screens/Library';
+import Home from '../screens/Beranda';
 import DetailPost from '../screens/DetailPost';
 
 const Stack = createStackNavigator();
 
-export const LibraryStack = () => {
+export const PostStack = () => {
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Library" component={Home} />
+            <Stack.Screen name="Post" component={Home} />
             <Stack.Screen name="DetailPost" component={DetailPost} />
         </Stack.Navigator>
     );
